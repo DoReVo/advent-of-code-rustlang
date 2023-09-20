c79661f2c2b70f7b65f441a2f2c27bf8c728bb0f
DoReVo <frz565@gmail.com>
2023-09-20 14:07:37 +0800

    feat: solved day 1 2015

--- src/aoc_2015/mod.rs
+++ src/aoc_2015/mod.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 pub fn day_1() {
-    println!("Hello from submod");
+    let raw_input = fs::read_to_string("src/aoc_2015/day1_input").unwrap();
+    let mut start_floor = 0;
+    let input_vec = raw_input.split("");
+    input_vec.for_each(|s| {
+        if s == "(" {
+            start_floor += 1;
+        } else if s == ")" {
+            start_floor -= 1;
+        };
+    });
+
+    println!("Final floor is {}", start_floor);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod aoc_2015;
 
 fn main() {
-    println!("Hello, world!");
     aoc_2015::day_1();
 }
