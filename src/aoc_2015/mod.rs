7b5d61e9ab1379ab11db46378a92cd0487ee1da5
DoReVo <frz565@gmail.com>
2023-09-27 13:58:37 +0800

    feat: done task 2 day 2

--- src/aoc_2015/mod.rs
+++ src/aoc_2015/mod.rs
@@ -13,7 +13,6 @@ pub fn day_1() {
             start_floor -= 1;
         };
 
-
         if start_floor == -1 && negative_floor.is_none() {
             negative_floor = Some(i);
         }
@@ -22,8 +21,8 @@ pub fn day_1() {
     println!("Final floor is {}", start_floor);
     println!("Negative floor is {}", negative_floor.unwrap());
 }
-
 #[derive(Debug)]
+
 struct Dimension {
     length: i32,
     width: i32,
@@ -61,15 +60,42 @@ pub fn day_2() {
             let total = 2 * (s_1 + s_2 + s_3);
             let smallest_side = std::cmp::min(s_1, std::cmp::min(s_2, s_3));
             let answer = total + smallest_side;
-            println!("Answer is {:?}", answer);
-            println!("Question is {:?}", d);
-            println!("Dim is {} {} {}", s_1, s_2, s_3);
             answer
         })
         .sum();
-    println!("{:?}", total_area);
-}
 
+    let total_ribbon: i32 = dimensions
+        .iter()
+        .map(|d| {
+            // Find perimeter for all sides
+            let top_bottom: i32 = d.length + d.length + d.width + d.width;
+            let left_right: i32 = d.width + d.width + d.height + d.height;
+            let front_back: i32 = d.height + d.height + d.length + d.length;
+
+            // Total ribbon to wrap present
+            let present_wrap_ribbon =
+                std::cmp::min(top_bottom, std::cmp::min(left_right, front_back));
+            // Total ribbon to use with bow
+            let bow_ribbon = d.length * d.width * d.height;
+
+            let total_ribbon = present_wrap_ribbon + bow_ribbon;
+
+            total_ribbon
+        })
+        .sum();
+    println!("Wrapping for gift required {:?}", total_area);
+    println!("Total ribbon required {:?}", total_ribbon)
+}
+pub fn day_3() {
+    let raw_input = fs::read_to_string("src/aoc_2015/day3_input").unwrap();
+
+    enum Direction {
+        North,
+        East,
+        South,
+        West
+    };
+}
 pub fn run_2015() {
-    day_1();
+    day_3();
 }
